f8a42fcbc25b259d728558721e5966618cb0b189
Jiří Zůna <jiri@zunovi.cz>
2016-12-06 16:20:45 +0100

    ADD Simple RestListing utility
    This class allows to fetch pages of information from APIs.
    It handles paging internally and is useful for infinite scroll listings.

---
+++ lib/src/rest_listing.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+import 'package:fnx_rest/src/rest_client.dart';
+
+/// Simple utility which allows to load pages of results for a given API endpoint
+///
+/// To load the results it requires configured [RestClient].
+///
+/// It accepts [RestListingDriver] which handles paging for requests and unpacking
+/// final List of data from returned response
+class RestListing {
+
+  RestClient rest;
+
+  List<dynamic> list = [];
+  int page = 0;
+
+  bool _hasNext = true;
+  int _version = 0;
+  RestListingDriver _driver;
+
+  RestListing(RestClient rest, RestListingDriver driver) {
+    this.rest = rest.child('');
+    this._driver = driver;
+  }
+
+  bool get working => rest.working;
+  bool get hasNext => _hasNext;
+
+  /// Use this to indicate that next page should be loaded.
+  Future<bool> loadNextPage() async {
+    if (working) {
+      return false;
+    }
+
+    RestClient client = _driver.prepareClient(rest, page);
+
+    int reqVersion = _version;
+    RestResult rr = await client.get();
+    if (_version != reqVersion) return false;
+
+    UnpackedData data = _driver.unpackData(rr.data);
+    page++;
+
+    if (data.data != null && data.data.isNotEmpty) {
+      list.addAll(data.data);
+    }
+    _hasNext = data.hasNext;
+
+    return true;
+  }
+
+  /// Clears accumulated data and starts fetching page 0
+  Future<bool> refresh() {
+    _version++;
+    list.clear();
+    _hasNext = true;
+    return loadNextPage();
+  }
+}
+
+/// Allows to hook into the process of requesting data from the API
+/// and then unwrapping List of results from provided response
+abstract class RestListingDriver {
+
+  /// Extract interesting data from the API response
+  UnpackedData unpackData(dynamic data);
+
+  /// Configure rest client so that it will request given `page` of data
+  RestClient prepareClient(RestClient client, int page);
+
+}
+
+/// Expects the API to return List of data directly in its response.
+class SimpleListDriver implements RestListingDriver {
+
+  @override
+  RestClient prepareClient(RestClient client, int page) {
+    return queryParamPager(client, page);
+  }
+
+  @override
+  UnpackedData unpackData(dynamic data) {
+    data = data ?? [];
+    return new UnpackedData(data.isNotEmpty, data);
+  }
+
+  static RestClient queryParamPager(RestClient client, int page) {
+    client.setParam('page', page.toString());
+    return client;
+  }
+}
+
+/// Expects the API to return a Map with 'data' attribute which contains
+/// the actual List of results
+class ListResultDriver implements RestListingDriver {
+
+  @override
+  RestClient prepareClient(RestClient client, int page) {
+    return SimpleListDriver.queryParamPager(client, page);
+  }
+
+  @override
+  UnpackedData unpackData(dynamic data) {
+    if (data is Map) {
+      if (data != null && data is! List) {
+        throw "Expected 'data' key to be a List, but was $data";
+      }
+      List<dynamic> list = data ?? [];
+      return new UnpackedData(list.isNotEmpty, list);
+    } else {
+      throw "Expected result to be a map with 'data' key containing results";
+    }
+  }
+}
+
+/// Contains the data itself and flag whether we think (or know) that
+/// there is more data to fetch (next page)
+class UnpackedData {
+  final bool hasNext;
+  final List<dynamic> data;
+
+  UnpackedData(this.hasNext, this.data);
+
+  UnpackedData.empty(): this(false, []);
+}
---
+++ test/rest_listing_test.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:fnx_rest/src/rest_client.dart';
+import 'package:fnx_rest/src/rest_listing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("SimpleListDriver", () {
+    group("when unpacking data, should", () {
+      SimpleListDriver simpleRestListingDriver = new SimpleListDriver();
+      test("set hasNext to true when data were accepted", () {
+        UnpackedData data = simpleRestListingDriver.unpackData([1, 2, 3]);
+        expect(data.hasNext, isTrue);
+      });
+      test("set hasNext to false when data is empty", () {
+        UnpackedData data = simpleRestListingDriver.unpackData([]);
+        expect(data.hasNext, isFalse);
+      });
+      test("return the whole response", () {
+        UnpackedData data = simpleRestListingDriver.unpackData([1, 2, 3]);
+        expect(data.data, equals([1, 2, 3]));
+      });
+    });
+    group("when preparing client", () {
+      test("include the page query parameter with given page", () {
+        RestClient dummyClient = new RestClient(null, null, "/");
+        SimpleListDriver driver = new SimpleListDriver();
+        RestClient c = driver.prepareClient(dummyClient, 10);
+        expect(c.params['page'], equals("10"));
+      });
+    });
+  });
+
+  group("ListResultDriver", () {
+    group("when unpacking data, should", () {
+      group("fail if", () {
+        test("response is not a map", () {
+          ListResultDriver driver = new ListResultDriver();
+          expect(() => driver.unpackData([1, 2, 3]),
+              throwsA((String msg) => msg.contains("Expected result to be a map")));
+        });
+        test("response has no property 'data'", () {
+          ListResultDriver driver = new ListResultDriver();
+          expect(() => driver.unpackData({'data': "string"}),
+              throwsA((String msg) => msg.contains("Expected 'data' key to be a List")));
+        });
+      });
+    });
+  });
+}
